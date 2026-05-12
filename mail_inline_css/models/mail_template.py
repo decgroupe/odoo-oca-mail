15e4910f83edca852e6c6fa0027290905ec95b5a
Yann Papouin <ypa@decgroupe.com>
2026-05-12 16:03:24 +0200

    CSS fix

--- mail_inline_css/models/mail_template.py
+++ mail_inline_css/models/mail_template.py
@@ -3,10 +3,14 @@
 
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import logging
+
+import cssutils
 from premailer import Premailer
 
 from odoo import models
 
+cssutils.log.setLevel(logging.CRITICAL)
 
 class MailTemplate(models.Model):
     _inherit = "mail.template"
