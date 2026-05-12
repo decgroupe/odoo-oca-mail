b618a3c44a9a934e47407388a3678cda4f12b2de
Yann Papouin <ypa@decgroupe.com>
2026-05-12 16:12:32 +0200

    [IMP] mail_activity_partner: pre-commit auto fixes

--- mail_activity_partner/__manifest__.py
+++ mail_activity_partner/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Social Network",
-    "website": "https://github.com/OCA/social",
+    "website": "https://github.com/OCA/mail",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
